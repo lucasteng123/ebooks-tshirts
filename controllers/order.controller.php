f608f309500672f2412ece4c94e7ba24fd6afe39
Lucas Teng <lucasteng@Lucass-MacBook-Pro.local>
2017-06-18 15:43:45 -0400

    Fixed order controller to include new design elements

--- controllers/order.controller.php
+++ controllers/order.controller.php
@@ -30,6 +30,7 @@ $methods[ 'run' ] = function ( $instance ) {
 			}
 			$tshirt = $result[ 0 ];
 			$colors = explode( ",", $tshirt[ "colors" ] );
+			$sizes = explode(",", $tshirt["size"]);
 
 			echo( '<!DOCTYPE html>
 <html>
@@ -41,17 +42,17 @@ $methods[ 'run' ] = function ( $instance ) {
 <body>
 <div class="container">
 	<div class="row">
-		<h1>' . $tshirt[ "name" ] . '</h1>
-		<hr>
-	</div>
-
-	<div class="row">
 		<div class="col-md-7">
 			<img src = "' . $tshirt[ "image" ] . '" class="img-thumbnail" />
 		</div>
 		<div class="col-md-5">
-			<h2>Order this shirt</h2>
+			<h1>' . $tshirt[ "name" ] . '</h1>
+			<h4 style="font-weight: 200">' . $tshirt[ "description" ] . '</h4>
 			<hr>
+			<div class="alert alert-warning"> 
+      	<strong>Tip:</strong> For order issues contact support@bestebooks.ca 
+      		</div>
+			
 			<h3><strong>$' . $tshirt[ "price" ] . '</strong><span style="font-weight: 200">/shirt</span></h3>
 			<form action="/order/place/' . $id . '" method="post">
 				<div class="form-group">
@@ -65,16 +66,45 @@ $methods[ 'run' ] = function ( $instance ) {
 					<input type="radio" class="form-control" style="display: inline-block;" value="' . $color . '" name="colors">
 				</div>' );
 			}
+			echo('</div><div class="row">
+         <div class="form-group">
+          <label class="control-label col-sm-3">Size</label>
+          <div class="text-right col-sm-9">
+            <div id="button1idGroup" class="btn-group" role="group" aria-label="Button Group">');
+			foreach ( $sizes as $size ) {
+				echo( '
+				<button type="button" id="button1id" name="button1id" class="btn btn-default" aria-label="Left Button">' . $size . '</button>' );
+			}
 			echo( '
 				
 				</div>
+            <p class="help-block">Select the size you wish to purchase</p>
+          </div>
+        </div>
+		</div>
 				<div class="form-group">
 					<label for="email"> Email Address </label>
 					<input type="text" class="form-control" name="email">
 				</div>
-				
+				<div class="alert alert-danger">
+					<a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+					<strong>Error:</strong> I will appear here if an error occurs
+				</div>
+				<div class="row">
+          <div class="col-md-9 col-xs-9">
+            <label for="quantity"> Quantity: </label>
+            <select name="quantity">');
+              for($i=1; $i<=10; $i++){
+				  echo('<option>'.$i.'</option>'); 
+			  }
+			echo('
+            </select>
+          </div>
+				<div class="col-md-3 col-xs-3">
 				<div class="form-group">
 					<input class="btn btn-primary" type="submit">
+				</div>
+				</div>
 				</div>
 			</form>	
 
